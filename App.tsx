cf6bbb857dc6e877b0aff02f7a17bda7b880ae33
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2021-06-24 21:10:28 -0300

    Config do App

--- App.tsx
+++ App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import {SignIn} from './src/screens/SignIn'
+import React, { Fragment } from 'react';
+import {StatusBar} from 'react-native';
+import {useFonts} from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani';
+
+import {Background} from './src/components/Background'
+import {Routes} from './src/routes'
 
 export default function App(){
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return(
-    <SignIn/>
+      <Background>
+          <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+          <Routes/>
+      </Background>
+      
+     
+    
+    
   )
 };
