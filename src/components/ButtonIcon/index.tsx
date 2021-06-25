5f2cdab6a90cc0c049e1edfc7bdd9186ea554824
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2021-06-24 21:33:47 -0300

    utilizando RectButton

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {Text, View, Image, TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import {RectButton, RectButtonProps} from 'react-native-gesture-handler'
 
 import DiscordImg from '../../assets/discord.png';
 import {styles} from './styles';
 
-type Props = {
+type Props = RectButtonProps & {
     title: string;
+    
 }
 
-export function ButtonIcon({title} : Props){
+export function ButtonIcon({title, ...rest} : Props){
     return (
-        <TouchableOpacity style={styles.container}>
+        <RectButton style={styles.container} {...rest}>
             <View style={styles.iconWrapper}>
                 <Image source={DiscordImg} style={styles.icon} />
             </View>
@@ -18,6 +20,6 @@ export function ButtonIcon({title} : Props){
             <Text style={styles.title}>
                 {title} 
             </Text>
-        </TouchableOpacity>
+        </RectButton>
     )
 }
