c57c24725c6d2e029898023505f83baa7a2a0f40
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2021-06-24 21:06:47 -0300

    componente do avatar

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container:{
+        height: 50,
+        width: 50,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 22,
+    },
+    avatar:{
+        height: 46,
+        width: 46,
+        borderRadius: 8,
+    }
+})
